ceea18fac193b8102ef0ffdf60a1ec25ed7a915c
Tiago Ilieve <tiago.myhro@gmail.com>
2016-04-03 13:45:54 -0300

    desktop: add 'light-locker', remove 'xscreensaver'
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=819703

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -1,9 +1,14 @@
 package 'chromium'
 package 'iceweasel'
 package 'kupfer'
+package 'light-locker'
 package 'parcellite'
 package 'pavucontrol'
 package 'ttf-mscorefonts-installer'
 package 'vim-gtk'
 package 'xclip'
 package 'xtightvncviewer'
+
+package 'xscreensaver' do
+    action :purge
+end
