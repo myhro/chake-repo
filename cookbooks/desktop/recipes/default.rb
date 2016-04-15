3cdcd7fbe603eaf9cfa0a5bbe0392cccfdbcd812
Tiago Ilieve <tiago.myhro@gmail.com>
2016-04-15 06:41:12 -0300

    desktop: add 'pasystray' and xfce4-whiskermenu

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -3,10 +3,12 @@ package 'iceweasel'
 package 'kupfer'
 package 'light-locker'
 package 'parcellite'
+package 'pasystray'
 package 'pavucontrol'
 package 'ttf-mscorefonts-installer'
 package 'vim-gtk'
 package 'xclip'
+package 'xfce4-whiskermenu-plugin'
 package 'xtightvncviewer'
 
 package 'xscreensaver' do
