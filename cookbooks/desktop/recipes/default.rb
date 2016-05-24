84316f0f104c4cf552c568498af2cb115b28a2de
Tiago Ilieve <tiago.myhro@gmail.com>
2016-05-24 09:15:57 -0300

    desktop: change 'xtightvncviewer' for 'remmina'
    Remmina also suports RDP besides VNC.

--- cookbooks/desktop/recipes/default.rb
+++ cookbooks/desktop/recipes/default.rb
@@ -5,11 +5,11 @@ package 'light-locker'
 package 'parcellite'
 package 'pasystray'
 package 'pavucontrol'
+package 'remmina'
 package 'ttf-mscorefonts-installer'
 package 'vim-gtk'
 package 'xclip'
 package 'xfce4-whiskermenu-plugin'
-package 'xtightvncviewer'
 
 package 'xscreensaver' do
     action :purge
