adf9f90fc8e9f0537f457800124714aa4b6801a0
Tiago Ilieve <tiago.myhro@gmail.com>
2016-07-03 15:17:13 -0300

    system/docker: add 'docker-clean.sh'

--- cookbooks/system/recipes/docker.rb
+++ cookbooks/system/recipes/docker.rb
@@ -1,1 +1,21 @@
 package 'docker.io'
+
+docker_clean = '/usr/local/bin/docker-clean.sh'
+
+docker_clean_src = "#!/bin/bash
+docker rm $(docker ps -a | grep 'Exited' | awk '{ print $1 }')
+docker rmi $(docker images | grep '^<none>' | awk '{ print $3 }')
+"
+
+file docker_clean do
+    content docker_clean_src
+    owner 'root'
+    group 'root'
+    mode '0775'
+end
+
+cron 'docker-clean' do
+    minute '0'
+    hour '6'
+    command docker_clean
+end
